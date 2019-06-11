d52a736f9146533763c2422f8cfd1d2069fc3bae
JeongKwangHee <sweetbaba12@naver.com>
2019-06-12 02:00:50 +0900

    rank

--- pages/rank.js
+++ pages/rank.js
@@ -1,7 +1,35 @@
 const Rank = () => {
   return (
     <div>
-      Rank rank
+      <ul className="rank-menu">
+        <li>Today</li>
+        <li>Outer</li>
+        <li>Top</li>
+        <li>Skirt</li>
+        <li>Pants</li>
+        <li>Dress</li>
+        <li>Shoes</li>
+      </ul>
+      <div className="newly-influencer">
+        <div className="face"></div>
+        <div className="name">real_brown</div>
+        <div className="star">364</div>
+      </div>
+      <div className="ranking">
+        <h1>Ranking</h1>
+        <div>
+          <div>
+            <div className="face"></div>
+            <div className="name-star">
+              <p>real_brown</p>
+              <p>498.3k</p>
+            </div>
+            <div className="follow">
+              Following
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
