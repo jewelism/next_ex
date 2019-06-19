e0364be061d08775ef7c0ffbf36e8e6b3668b72c
jewelism <boseokjung@gmail.com>
2019-06-19 18:21:47 +0900

    refactor main menu

--- components/Menu/MainMenu.js
+++ components/Menu/MainMenu.js
@@ -1,27 +1,26 @@
 import Link from 'next/link';
+import '../../styles/menu.scss';
 
+const menuList = [
+  { path: '', text: 'News Feed' },
+  { path: 'board/notice', text: 'Notice' },
+  { path: 'board/event', text: 'Event' },
+  { path: 'rank', text: 'Rank' },
+  //
+  { path: 'test', text: 'Test' },
+  { path: 'about', text: 'About' },
+  { path: 'payment', text: 'Payment' },
+  { path: 'product', text: 'Product' },
+];
 const MainMenu = () => (
-  <ul>
-    <li>
-      <Link href="/about">
-        <a>About Page</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/payment">
-        <a>Payment Page</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/product">
-        <a>Product Page</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/test">
-        <a>Test Page</a>
-      </Link>
-    </li>
+  <ul className="main-menu-ul">
+    {menuList.map(m =>
+      <li key={m.text} className="main-menu-li">
+        <Link href={`/${m.path}`}>
+          <a>{m.text}</a>
+        </Link>
+      </li>
+    )}
   </ul>
 );
 
